6b187a5fe114f4f72bd6f73778caf039499e5f27
RavidAccion <ravid.rv@accionlabs.com>
2023-01-27 17:42:32 +0530

    crud

--- ASPAPP/Controllers/CategoryController1.cs
+++ ASPAPP/Controllers/CategoryController1.cs
@@ -1,6 +1,7 @@
 ﻿using ASPAPP.Data;
 using ASPAPP.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPAPP.Controllers
 {
@@ -15,6 +16,69 @@ namespace ASPAPP.Controllers
         {
 IEnumerable<Category> ObjCatList=_db.categoryTab1;
             return View(ObjCatList);
+        }
+        //for get
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        //post
+        [HttpPost]//want to add for post 
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.categoryTab1.Add(obj);//to add the obj to db
+            _db.SaveChanges();//to save the changes in db
+            return RedirectToAction("Index");
+        }
+
+        //for get
+        public IActionResult Edit(int ?id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var catfromdb= _db.categoryTab1.Find(id);
+            if(catfromdb==null)
+            {
+                return NotFound();
+            }
+            return View(catfromdb);
+        }
+        //post
+        [HttpPost]//want to add for post 
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            _db.categoryTab1.Update(obj);//to add the obj to db
+            _db.SaveChanges();//to save the changes in db
+            return RedirectToAction("Index");
+        }
+
+        //for get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var catfromdb1 = _db.categoryTab1.Find(id);
+            if (catfromdb1 == null)
+            {
+                return NotFound();
+            }
+            return View(catfromdb1);
+        }
+        //post
+        [HttpPost]//want to add for post 
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+            _db.categoryTab1.Remove(obj);//to add the obj to db
+            _db.SaveChanges();//to save the changes in db
+            return RedirectToAction("Index");
         }
     }
 }
